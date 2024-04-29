9c9eb6a606bcf00d43932a4e828a048195e1a70b
Your Name <rishabhyadav70070@gmail.com>
2024-04-30 00:29:08 +0530

    computed prop.

---
+++ comp.js
@@ -0,0 +1,15 @@
+//computed properties
+
+const key1 = "object1";
+const key2 = "object2";
+
+const value1 = "myvalue1";
+const value2 = "myvalue2";
+
+const obj = {
+    [key1]: value1,
+    [key2]: value2
+}
+
+console.log(obj);
+
