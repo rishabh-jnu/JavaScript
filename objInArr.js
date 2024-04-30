590ed26b182dfeeea3422403fe6d5dade8fca636
Your Name <rishabhyadav70070@gmail.com>
2024-04-30 23:59:16 +0530

    Object in array

---
+++ objInArr.js
@@ -0,0 +1,11 @@
+//object inside array
+
+const users = [
+    {userId:1, firstName:'rishabh', gender: 'male'},
+    {userId:2, firstName: 'satyam', gender: 'male'}
+]
+
+for(let user of users)
+{
+    console.log(user);
+}
