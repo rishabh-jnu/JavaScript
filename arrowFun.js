294b52ee4e57d70463809520c246e059950cba0d
Your Name <rishabhyadav70070@gmail.com>
2024-05-27 09:22:36 +0530

    Arrow Func

---
+++ arrowFun.js
@@ -0,0 +1,15 @@
+//arrow func
+
+
+const singhappyBirthady = () =>{
+    console.log("happy Birthday");
+}    
+
+singhappyBirthady();
+
+const sumTwoNumbers = (num1, num2) =>{
+    return num1 + num2;
+}
+
+const sumOfTwoNum = sumTwoNumbers(4, 7);
+console.log(sumOfTwoNum);
